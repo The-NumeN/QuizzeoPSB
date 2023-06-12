b012d6740fc59edd7d1efd115a1563f118bdc3ac
poulpy666 <lilopolo912@gmail.com>
2023-06-12 15:26:23 +0200

    ajout coco

--- header.php
+++ header.php
@@ -24,7 +24,7 @@
             <a class="nav-link" href="connexion.php">Connexion</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="">Nous Connaitre</a>
+            <a class="nav-link" href="coco.php">Nous Connaitre</a>
           </li>
         </ul>
       </div>
