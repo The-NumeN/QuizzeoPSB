f09c7e1b668d62cb62a26c38ee4dbdc81bee65b3
Numen <stivegamy@outlook.fr>
2023-06-12 14:41:17 +0200

    form connect

---
+++ connexion.php
@@ -0,0 +1,60 @@
+<?php
+session_start();
+// On récupère les valeurs saisies
+if(isset($_POST["button"])){
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    // On se connecte à la base de donnée 
+    $connect_bdd = mysqli_connect("127.0.0.1","root","","quizzeo");
+
+    // On lance la requête de récupération des valeurs saisies
+    $req = "SELECT * FROM Users WHERE pseudo = '$username' AND password = '$password'";
+    $res = mysqli_query($connect_bdd, $req);
+    
+    // On fait une condition au cas où les données saisies sont correctes ou fausses
+    if(mysqli_num_rows($res) > 0){
+        $row = mysqli_fetch_assoc($res);
+        $_SESSION["username"] = $username;
+        // Vérification du rôle admin
+        if ($row["role"] == "admin") {
+            $_SESSION["role"] = "admin";
+            header("location: admin.php");
+        }
+        // Vérification du rôle quizzer
+        elseif ($row["role"] == "quizzer") {
+            $_SESSION["role"] = "quizzer";
+            header("location: quizzer.php");
+        }
+        // Redirection vers la page utilisateur par défaut
+        else {
+            header("location: user.php");
+        }
+    } else {
+        echo "Erreur";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Connexion.css">
+    <title>Connexion</title>
+</head>
+<body>
+    <div>
+    <form action="" method="post">
+        <h1>Connexion</h1><hr>
+        <label>Identifiant</label><br>
+        <input type="text" name="username" required  ><br>
+        <label>Mot de passe</label><br>
+        <input type="password" name="password" required ><br>
+        <button type="submit" name="button">Se connecter</button>
+        <a href="Form_connect.php">Créer un compte</a>
+    </form>
+    </div>
+</body>
+</html>
