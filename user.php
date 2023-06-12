8a7b90b25967067e5980d2b8bae977916d33e28b
poulpy666 <lilopolo912@gmail.com>
2023-06-12 16:02:39 +0200

    b

--- user.php
+++ user.php
@@ -7,7 +7,7 @@ include 'header.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Quizz</title>
+    <title>Joueur</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <script></script>
     <script></script>
