7b06724266a3734ae140d510479e031cc04ed64a
poulpy666 <lilopolo912@gmail.com>
2023-06-12 15:17:49 +0200

    modifco

--- connexion.php
+++ connexion.php
@@ -50,11 +50,10 @@ if(isset($_POST["button"])){
     <form action="" method="post">
         <h1>Connexion</h1><hr>
         <label>Pseudo</label><br>
-        <input type="text" name="username" required  ><br>
+        <input type="text" name="username" required  ><br><br>
         <label>Mot de passe</label><br>
-        <input type="password" name="password" required ><br>
+        <input type="password" name="password" required ><br><br>
         <button type="submit" name="button">Se connecter</button>
-        <a href="inscription.php">Créer un compte</a>
     </form>
     </div>
 </body>
