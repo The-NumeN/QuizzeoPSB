01e4bacad4b5f72ade04eb957b5751702c8356dd
poulpy666 <lilopolo912@gmail.com>
2023-06-12 11:52:35 +0200

    dl

--- IndexQ.php
+++ IndexQ.php
@@ -23,7 +23,7 @@
     <meta charset="UTF-8">
     <title>Login</title>
     <link rel="stylesheet" href="Quizz.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" c> 
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N"> 
 </head>
 <body>
     <nav class="navbar navbar-dark bg-success">
