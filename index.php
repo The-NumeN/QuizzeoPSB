67ef68b4ca958ebd7bc2c8d8cc27baf146596804
poulpy666 <lilopolo912@gmail.com>
2023-06-12 14:26:08 +0200

    av.bootstrap

--- index.php
+++ index.php
@@ -1,13 +1,19 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Quizzeo</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
 <body>
-    <h1> Bienvenue sur Notre Quizz<h1>
+    <div class="titre">
+        <h1> Bienvenue sur Notre Quizz<h1> <br><br>
+    </div>
+    <div class="para">
+        <p>Sur ce site, vous pouvez tester vos connaissances en jouant à des quizz ou bien en les créant (après avoir réussi plusieurs quizz).</p> <br><br>
+    </div>
+
     
 </body>
 </html>
