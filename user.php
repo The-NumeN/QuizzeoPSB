ffe72af51ef7bb2d9c247c10173c0ccb8580a18b
poulpy666 <lilopolo912@gmail.com>
2023-06-12 15:59:47 +0200

    b

--- user.php
+++ user.php
@@ -1,3 +1,18 @@
-<?php 
-include "header.php";
+
+<?php
+include 'header.php';
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quizz</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <script></script>
+    <script></script>
+    <script></script>
+    <script></script>
+</head>
+<body>
+</body>
