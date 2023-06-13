f2a3b0f6ff94ebed03044e37d88755dafec9dee6
poulpy666 <lilopolo912@gmail.com>
2023-06-13 11:21:40 +0200

    classes

---
+++ classes_POO.php
@@ -0,0 +1,38 @@
+<?php
+class utilisateur {
+    private $pseudo;
+    private $mail;
+    private $password;
+    private $role;
+
+    public function __construct(){
+        $this->pseudo=$ps;
+        $this->mail=$mail;
+        $this->password=$pwd;
+        $this->role=$rl;
+    }
+}
+class Quizz{
+    private $titre;
+    private $difficulte;
+    private $date;
+
+public function __construct(){
+    $this->titre=$titre;
+    $this->difficulte=$diff;
+    $this->$date=$date;
+}
+}
+class Question{
+    private $intitule;
+    private $difficulte;
+    private $date;
+
+public function __construct(){
+    $this->intitule=$int;
+    $this->diffficulte=$diff;
+    $this->date=$date;
+}
+}
+
+?>
