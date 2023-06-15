ba54262f8091c395be94c6c8c4da702b413418a9
poulpy666 <lilopolo912@gmail.com>
2023-06-15 11:10:42 +0200

    POOquestions

--- classes_POO.php
+++ classes_POO.php
@@ -1,24 +1,56 @@
 <?php
-class Quizz{
-    private $titre;
-    private $difficulte;
-    private $date;
+    class Quizz{
+        private $host="127.0.0.1";
+        private $username="root";
+        private $password="";
+        private $database="quizzeo";
+        private $titre;
+        private $difficulte;
+        private $date;
 
-public function __construct(){
-    $this->titre=$titre;
-    $this->difficulte=$diff;
-    $this->$date=$date;
-}
-}
-class Question{
-    private $intitule;
-    private $difficulte;
-    private $date;
+        public function __construct(){
+    
+            $this->host=$host;
+            $this->username=$username;
+            $this->password=$pwd;
+            $this->database=$bdd;
+            $this->titre=$titre;
+            $this->difficulte=$diff;
+            $this->$date=$date;
 
-public function __construct(){
-    $this->intitule=$int;
-    $this->diffficulte=$diff;
-    $this->date=$date;
-}
-}
+            {
+                $this->conn = msqli_connect($this->host,$this->username,$this->password,$this->database);
+                if($this->conn->connect_error){
+                    echo "Echec de la connexion à la base de données:".$this->conn->connect_error;
+                }
+            }
+        
+        }
+    }
+
+    class Question{
+        private $host="127.0.0.1";
+        private $username="root";
+        private $password="";
+        private $database="quizzeo";
+        private $intitule;
+        private $difficulte;
+        private $date;
+
+        public function __construct(){
+            $this->host=$host;
+            $this->username=$username;
+            $this->password=$pwd;
+            $this->database=$bdd;
+            $this->intitule=$int;
+            $this->diffficulte=$diff;
+            $this->date=$date;
+            {
+                $this->conn = msqli_connect($this->host,$this->username,$this->password,$this->database);
+                if($this->conn->connect_error){
+                    echo "Echec de la connexion à la base de données:".$this->conn->connect_error;
+                }
+            }
+        }
+    }
 ?>
