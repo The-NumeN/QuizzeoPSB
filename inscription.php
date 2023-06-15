c77a0ca3d73f8652ace373bb163bd29a4faec41e
poulpy666 <lilopolo912@gmail.com>
2023-06-15 14:21:36 +0200

    csc

--- inscription.php
+++ inscription.php
@@ -28,7 +28,7 @@
     <meta charset="UTF-8">
     <title>Login</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N"> 
-    <link rel="stylesheet" href="conect2.css">
+    <link rel="stylesheet" href="connect2.css">
 </head>
 <body>
     <form action="" method="post"> 
