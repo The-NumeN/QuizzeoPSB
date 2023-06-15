78f7d6acbef0caba8dfb138d5c67a77f7d6784b4
poulpy666 <lilopolo912@gmail.com>
2023-06-15 14:25:14 +0200

    csc

--- header.php
+++ header.php
@@ -26,9 +26,6 @@
           <li class="nav-item">
             <a class="nav-link" href="connexion.php">Connexion</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="coco.php">Nous Connaitre</a>
-          </li>
         </ul>
       </div>
     </div>
