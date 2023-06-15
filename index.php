a0068d5184073f16c9c657f7c2bc2ef6caa0395b
poulpy666 <lilopolo912@gmail.com>
2023-06-15 14:24:14 +0200

    csc

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ include 'header.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Quizzeo</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="connect2.css">
     
 <body>
     <div class="titre">
@@ -16,6 +17,9 @@ include 'header.php';
     </div>
     <div class="para">
         <p>Sur ce site, vous pouvez tester vos connaissances en jouant à des quizz ou bien en les créant (après avoir réussi plusieurs quizz).</p> <br><br>
-</body>
+
+        <h2>Nous Connaitre</h2>
+
+<p> Nous sommes une équipe de trois développeurs ayant crée ce site en "INSERER DUREE" pour "INSERER NOM CLIENT". <br> Dans l'équipe il y a Stive[DECRIS TOI]. <br><br> Il y a aussi Babacar[DECRIS TOI]. <br><br> Et Finalement, Paul, 25ans développeur web depuis 6mois.</p>    </body>
 </html>
 
