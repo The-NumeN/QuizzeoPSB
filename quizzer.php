98b6ad50037a893eb0c60599d854dad15c1138c8
poulpy666 <lilopolo912@gmail.com>
2023-06-19 14:12:24 +0200

    bip

--- quizzer.php
+++ quizzer.php
@@ -16,6 +16,9 @@ if (isset($_POST['logout']) && $_POST['logout'] === 'true') {
 <!DOCTYPE html>
 <html>
 <head>
+<meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <title>Quizzer</title>
     <link rel="stylesheet" href="connect2.css">
 </head>
