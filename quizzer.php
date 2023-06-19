24881da41657eab0bbb7d3a0df7145f3b0ac5500
poulpy666 <lilopolo912@gmail.com>
2023-06-19 11:26:59 +0200

    sheiB

--- quizzer.php
+++ quizzer.php
@@ -27,10 +27,10 @@ if (isset($_POST['logout']) && $_POST['logout'] === 'true') {
     <h1>Bonjour <span><?php echo ucfirst($_SESSION["pseudo"]); ?></span> , Bienvenue !</h1><hr>
 
     <h3>Liste des quizz</h3>
-    <a href="quizz_list.php">Voir la liste des quizz</a>
+    <a href="quizz_list.php">Voir la liste des quizz</a> <br><br>
 
     <h3>Ajouter un quizz</h3>
-    <a href="ajout_quiz.php">Ajouter un quizz</a>
+    <a href="ajout_quiz.php">Ajouter un quizz</a><br><br>
 
     <h3>Quizz créés par le quizzeur</h3>
     <a href="user_quizzes.php">Voir les quizz créés par le quizzeur</a>
