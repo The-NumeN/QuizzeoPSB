1e5e2de82733bedd103c749938ff20d2e2960be8
Babacar2804 <136070296+Babacar2804@users.noreply.github.com>
2023-06-19 12:43:51 +0200

    ajout

--- Merde.js
+++ Merde.js
@@ -1,10 +1,9 @@
 function addquizz() {
     // Cette fonction permet de créer un bouton qui ouvre un formulaire de création de quizz
-   
-    for(i=0;i<=5;i++){
+    for(i=1;i<=3;i++){
     var addquest = document.getElementById('crea').innerHTML;
     // au onclick on appelle une autre fonction qui ajoute des questions dans le quizz
-    addquest = addquest + "Question"+ i +'<br></br><form><label for="quest">Intitulé</label><input type="text"><br><label for="rep">Réponses:</label><input type="text" id="" placeholder="Bonne réponse"><br><input type="text" id="" placeholder="Mauvaise réponse"><br><input type="text" id="" placeholder="Mauvaise réponse"><br></form>';
+    addquest = addquest + "Question"+ i +'<br></br><form><input type="text" id="id_question" placeholder="Intitulé"><br><input type="text" id="bonne_reponse" placeholder="Bonne réponse"><br><input type="text" id="reponse" placeholder="Mauvaise réponse"><br><input type="text" id="reponse" placeholder="Mauvaise réponse"><br></form>';
     document.getElementById('crea').innerHTML = addquest;}
 }
 
