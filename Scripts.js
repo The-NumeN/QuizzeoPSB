ff464a12b9cd491880bc1bf102333fab11ad53ae
poulpy666 <lilopolo912@gmail.com>
2023-06-20 14:25:14 +0200

    bleah

--- Scripts.js
+++ Scripts.js
@@ -1,17 +1,17 @@
-var ident=1; 
+var ident = 1;
 function addquest() {
     // Cette fonction permet de créer un bouton qui ouvre un formulaire de création de quizz
-    const ajou=document.querySelectorAll('.gta');   
-    for(var ide=1;ide<=ajou.lenght;ide++){
-        ident=ide;
+    const ajou = document.querySelectorAll('.gta');
+    for (var ide = 1; ide <= ajou.lenght; ide++) {
+        ident = ide;
     }
     ident++
     var addquest = document.getElementById('crea').innerHTML;
     // au onclick on appelle une autre fonction qui ajoute des questions dans le quizz
-    addquest = addquest   +'<br></br><div id="gta"><form><input type="text" id="id_question'+ident+'"placeholder="Intitulé"><br><input type="text" id="bonne_reponse'+ident+'" placeholder="Bonne réponse"><br><input type="text" id="reponse1-'+ident+'" placeholder="Mauvaise réponse "><br><input type="text" id="reponse2-'+ident+'" placeholder="Mauvaise réponse"><br></form></div> ';
+    addquest = addquest + '<div id="gta"><br></br><form><input type="text" id="id_question' + ident + '"placeholder="Intitulé"><br><input type="text" id="bonne_reponse' + ident + '" placeholder="Bonne réponse"><br><input type="text" id="reponse1-' + ident + '" placeholder="Mauvaise réponse "><br><input type="text" id="reponse2-' + ident + '" placeholder="Mauvaise réponse"><br></form></div> ';
     document.getElementById('crea').innerHTML = addquest;
 }
-function suppquest(){
-    const element= document.getElementById("gta");
+function suppquest() {
+    const element = document.getElementById("gta");
     element.remove();
 }
