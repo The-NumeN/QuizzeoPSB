93673bb69949c02e93c9040b07d49517f5200658
Numen <stivegamy@outlook.fr>
2023-06-20 11:21:31 +0200

    All

--- user.php
+++ user.php
@@ -1,7 +1,4 @@
 <?php
-    include 'header.php';
-?>
-<?php
 session_start();
 
 if (isset($_POST['logout']) && $_POST['logout'] === 'true') {
@@ -25,16 +22,40 @@ if (!isset($_SESSION["pseudo"]) && $_SESSION["role"] !== "utilisateur") {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Joueurs</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="connect2.css">
-        <script src="creaquestion.js" type="text/javaScript"></script>
     </head>
     <body>
-        <form action="" method="post">
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+      <div class="container-fluid">
+      <!-- ajout du logo (retour au menu principal lorsque l'on clique dessus) -->
+        <a href="index.php"><img class="navbar-brand" src="img/logo-quiz-symboles-bulle-dialogue-concept-spectacle-questionnaire-chante-bouton-quiz-concours-questions-examen-embleme-moderne-interview_180786-72.avif" width="75" height="75" class="d-inline-block align-top" alt="Erreur"></a>
+        
+        <div class="navbar" id="navbarNav">
+          <ul class="navbar-nav  ">
+          <!-- ajout des liens de redirection -->
+          <div class="inscri">
+            <li class="nav-item">
+            <br><p class="bonjour">Bonjour <span><?php echo ucfirst($_SESSION["pseudo"]); ?></span>, Bienvenue !</p>
+            </li>
+          </div>
+          <div class="conn">
+            <li class="nav-item">
+            <form action="" method="post">
             <input type="hidden" name="logout" value="true">
             <button type="submit">Déconnexion</button>
         </form>
-    <h1>Bonjour <span><?php echo ucfirst($_SESSION["pseudo"]); ?></span>, Bienvenue !</h1><br>
+            </li>
+          </div>
+            <div class="deco">
+              <li class="nav-item">
+              </li>
+            </div>
+          </ul>
+        </div>
+      </div>
+    </nav>
     <h2>Cliquez sur le bouton pour jouer</h2>
         <div class="jouer">
             <input type="button"onclick="selectquizz()"value="Jouer"/><br><br>
