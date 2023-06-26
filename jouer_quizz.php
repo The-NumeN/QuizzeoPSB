3c28bef4a1868cd007e4e8a2a9467e7bb83ae860
Babacar2804 <136070296+Babacar2804@users.noreply.github.com>
2023-06-26 15:38:49 +0200

    ajout

--- jouer_quizz.php
+++ jouer_quizz.php
@@ -3,20 +3,28 @@ session_start();
 $connect_bdd = mysqli_connect("127.0.0.1", "root", "", "quizzeo");
 $stp=$_GET['id_quizz'];
 $test="SELECT * FROM questions where id_quizz='$stp'";
+$sql = "SELECT bonne_reponse, reponse,reponce ,reponze FROM choices";
 $result = mysqli_query($connect_bdd, $test);
-$question = [];
-while ($row = mysqli_fetch_assoc($result)) {
-    $question[] = $row;
-  }
+$resulte = mysqli_query($connect_bdd, $sql);
 ?>
-
 <h2>Liste des questions</h2>
-    <table>
-        <?php foreach ($question as $quest) : ?>
-            <tr>
-                <td><?php echo $quest['intitule']; ?></td>
-                    
-                
-            </tr>
-        <?php endforeach; ?>
-</table>
+<?php 
+   if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        
+      echo  $row['intitule']."<br>";
+    }
+  } else {
+    echo "0 results";
+  }
+  if ($resulte->num_rows > 0) {
+    // output data of each row
+    while($row = $resulte->fetch_assoc()) {
+        
+      echo  $row["bonne_reponse"]."<br>". $row["reponse"]."<br>". $row["reponce"]."<br>".$row["reponze"];
+    }
+  } else {
+    echo "0 results";
+  }
+  ?>
