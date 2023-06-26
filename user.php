a30ba09bc246670da64edf64582e899ad8036bc0
Babacar2804 <136070296+Babacar2804@users.noreply.github.com>
2023-06-26 11:27:55 +0200

    ajout

--- user.php
+++ user.php
@@ -38,47 +38,35 @@ while ($row = mysqli_fetch_assoc($result)) {
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="connect2.css">
     </head>
-<<<<<<< HEAD
-  <body>
-      <nav class="navbar navbar-expand navbar-dark bg-dark">
-        <div class="container-fluid">
-          <!-- ajout du logo (retour au menu principal lorsque l'on clique dessus) -->
-          <a href="index.php"><img class="navbar-brand" src="img/logo-quiz-symboles-bulle-dialogue-concept-spectacle-questionnaire-chante-bouton-quiz-concours-questions-examen-embleme-moderne-interview_180786-72.avif" width="75" height="75" class="d-inline-block align-top" alt="Erreur"></a>      
-          <div class="navbar" id="navbarNav">
-            <ul class="navbar-nav  ">
-              <!-- ajout des liens de redirection -->
-              <div class="inscri">
-                <li class="nav-item">
-                  <br><p class="bonjour">Bonjour <span><?php echo ucfirst($_SESSION["pseudo"]); ?></span>, Bienvenue !</p>
-                </li>
-              </div>
-              <div class="conn">
-                <li class="nav-item">
-                  <form action="" method="post">
-                    <input type="hidden" name="logout" value="true">
-                    <button type="submit">Déconnexion</button>
-                  </form>
-                </li>
-              </div>
-            </ul>
-          </div>
-        </div>
-    </nav>
-  <br><br>
-    <div class="container">
-      <div class="card bg-light">
-        <div class="card-header">
-          <h2>Selectionnez un Quizz pour jouer</h2>
-        </div>
-        <div class="card-body">
-          <div class="card bg-light">
-            <div class="card-body">
-            <h3>Afficher les Quizz ici</h3>
-              <!-- METTRE LES QUIZZ ICI -->
-              <div class="jouer">
-            <input type="button" onclick="selectquizz()"value="Jouer"/><br><br>
-            <div id="play"></div>
-          </div>
+
+    <body>
+        <!-- HEADER -->
+        <nav class="navbar navbar-expand navbar-dark bg-dark">
+            <div class="container-fluid">
+            <!-- ajout du logo (retour au menu principal lorsque l'on clique dessus) -->
+                <a href="index.php"><img class="navbar-brand" src="img/logo-quiz-symboles-bulle-dialogue-concept-spectacle-questionnaire-chante-bouton-quiz-concours-questions-examen-embleme-moderne-interview_180786-72.avif" width="75" height="75" class="d-inline-block align-top" alt="Erreur"></a>
+                <div class="navbar" id="navbarNav">
+                    <ul class="navbar-nav  ">
+            <!-- ajout des liens de redirection -->
+                        <div class="inscri">
+                            <li class="nav-item">
+                                <br><p class="bonjour">Bonjour <span><?php echo ucfirst($_SESSION["pseudo"]); ?></span>, Bienvenue !</p>
+                            </li>
+                        </div>
+                        <div class="conn">
+                            <li class="nav-item">
+                                <form action="" method="post">
+                                    <input type="hidden" name="logout" value="true">
+                                    <button type="submit">Déconnexion</button>
+                                </form>
+                            </li>
+                        </div>
+                        <div class="deco">
+                            <li class="nav-item">
+                            </li>
+                        </div>
+                    </ul>
+                </div>
             </div>
         </nav>
     <h2>Liste des quizz</h2>
@@ -93,7 +81,7 @@ while ($row = mysqli_fetch_assoc($result)) {
                 <td><?php echo $quiz['titre']; ?></td>
                 <td><?php echo $quiz['difficulte']; ?></td>
                 <td>
-                    <a href="jouer_quizz.php?id_quizz=<?php echo $quiz['id_quizz']; ?>">Jouer</a>
+                    <a href="modif_quizz.php?id_quizz=<?php echo $quiz['id_quizz']; ?>">Jouer</a>
                 </td>
             </tr>
         <?php endforeach; ?>
