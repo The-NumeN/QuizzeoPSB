3f7c8358381ba4b8715f636b83f3b4eba78dcd0b
Babacar2804 <136070296+Babacar2804@users.noreply.github.com>
2023-06-26 12:53:39 +0200

    ajout

--- jouer_quizz.php
+++ jouer_quizz.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
 $connect_bdd = mysqli_connect("127.0.0.1", "root", "", "quizzeo");
-$test="SELECT * FROM questions";
+$stp=$_GET['id_quizz'];
+$test="SELECT * FROM questions where id_quizz='$stp'";
 $result = mysqli_query($connect_bdd, $test);
 $question = [];
 while ($row = mysqli_fetch_assoc($result)) {
@@ -10,11 +11,11 @@ while ($row = mysqli_fetch_assoc($result)) {
 ?>
 <h2>Liste des questions</h2>
     <table>
-        <?php foreach ($question as $quiz) : ?>
+        <?php foreach ($question as $quest) : ?>
             <tr>
-                <td><?php echo $quiz['intitule']; ?></td>
+                <td><?php echo $quest['intitule']; ?></td>
                     
-                </td>
+                
             </tr>
         <?php endforeach; ?>
 </table>
