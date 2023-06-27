809412bd4999e73279102257bf3eb1ff3b31e0d4
Numen <stivegamy@outlook.fr>
2023-06-27 15:39:27 +0200

    +++

--- jouer_quizz.php
+++ jouer_quizz.php
@@ -1,71 +1,64 @@
 <?php
 session_start();
-if (!isset($_SESSION["pseudo"]) && $_SESSION["role"] !== "utilisateur") {
-    header("location: Connexion.php");
-    exit();
-}
+$connect_bdd = mysqli_connect("127.0.0.1", "root", "", "quizzeo");
+$stp = $_GET['id_quizz'];
 
-if (isset($_GET['id_quizz'])) {
-    // Récupérer l'identifiant du quiz sélectionné depuis l'URL
-    $id_quizz = $_GET['id_quizz'];
+// Sélection des questions
+$test = "SELECT * FROM questions WHERE id_quizz='$stp'";
+$result = mysqli_query($connect_bdd, $test);
 
-    function BDDconnect() {
-        $connect_bdd = mysqli_connect("127.0.0.1", "root", "", "quizzeo");
-        if (!$connect_bdd) {
-            die("Échec de la connexion à la base de données: " .mysqli_error($connect_bdd));
-        }
-        return $connect_bdd;
-    }
-
-    $connect = BDDconnect();
-
-    $query = "SELECT * FROM Questions WHERE id_quizz = $id_quizz";
-    $result = mysqli_query($connect, $query);
-    $questions = [];
-
-    while ($row = mysqli_fetch_assoc($result)) {
-        $questions[] = $row;
-    }
-
-    foreach ($questions as &$question) {
-        $id_question = $question['id_question'];
-        $query_reponses = "SELECT * FROM choices WHERE id_question = $id_question";
-        $result_reponses = mysqli_query($connect, $query_reponses);
-        $reponses = [];
-
-        while ($row = mysqli_fetch_assoc($result_reponses)) {
-            $reponses[] = $row;
-        }
-        $question['reponses'] = $reponses;
-    }
-} else {
-
-    echo "Aucun quiz n'est sélectionné.";
-    exit();
-}
 ?>
-<!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Questions du Quiz</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <title>Connexion</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="connect2.css">
 </head>
-<body>
-    <h2>Questions du Quiz</h2>
 
-    <?php foreach ($questions as $question) : ?>
-        <h3><?php echo $question['intitule']; ?></h3>
-        <ul>
-            <?php foreach ($question['reponses'] as $reponse) : ?>
-                <button><?php echo $reponse['bonne_reponse']; ?></button><br><br>
-                <button><?php echo $reponse['reponse']; ?></button><br><br>
-                <button><?php echo $reponse['reponce']; ?></button><br><br>
-                <button><?php echo $reponse['reponze']; ?></button>
-            <?php endforeach; ?>
-        </ul>
-    <?php endforeach; ?>
-</body>
+<div class="cuicuiz">
+    <div class="liquest">
+        <h2>Liste des questions</h2>
+    </div>
+    <div class="container">
+        <?php
+        if ($result->num_rows > 0) {
+            // Parcourir les questions
+            while ($row = $result->fetch_assoc()) {
+                $questionId = $row['id_question'];
+                $questionText = $row['intitule'];
+
+                // Sélection des réponses pour la question actuelle
+                $sql = "SELECT * FROM choices WHERE id_question='$questionId'";
+                $resulte = mysqli_query($connect_bdd, $sql);
+                ?>
+                <div class="card bg-light">
+                    <div class="card-header">
+                        <?php echo $questionText; ?>
+                    </div>
+                    <div class="card-body">
+                        <?php
+                        if ($resulte->num_rows > 0) {
+                            // Parcourir les réponses de la question actuelle
+                            while ($row = $resulte->fetch_assoc()) {
+                                echo $row["bonne_reponse"] . "<br>" . $row["reponse"] . "<br>" . $row["reponce"] . "<br>" . $row["reponze"];
+                            }
+                        } else {
+                            echo "Aucune réponse trouvée.";
+                        }
+                        ?>
+                    </div>
+                </div>
+                <?php
+            }
+        } else {
+            echo "Aucune question trouvée.";
+        }
+        ?>
+    </div>
+</div>
+
 </html>
