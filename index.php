45963187300858c836b92432512e56b396162360
poulpy666 <lilopolo912@gmail.com>
2023-06-28 15:26:03 +0200

    loki

--- index.php
+++ index.php
@@ -20,11 +20,13 @@ include 'header.php';
                         <div class="titre">
                             <h1> Bienvenue sur Notre Quizz<h1>
                         </div>
+                        <br>
                         <p>Sur ce site, vous pouvez tester vos connaissances en jouant à des quizz ou bien en les créant (après avoir réussi plusieurs quizz).</p>
                     </div>
                     <div class="card-body">
                         <div class="para">                  
                             <h2>Nous Connaitre</h2>
+                            <br>
                             <p> Nous sommes une équipe de trois développeurs ayant crée ce site en un mois pour M.Peon <br><br> Dans l'équipe il y a Stive, 23 ans, le crack de la BDD et du PHP. <br><br> Il y a aussi Babacar, 19 ans, il s'occupe très bien du JS (on le complimente pas trop sinon il prends le melon ^^). <br><br> Et Finalement, Paul, 25ans qui s'occupe pas trop mal du css et de bootstrap.</p>
                         </div>
                     </div>
