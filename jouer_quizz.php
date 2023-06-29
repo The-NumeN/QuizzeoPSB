0399e29d59ab6a5a73012e66cfc1396c2df43c04
Numen <stivegamy@outlook.fr>
2023-06-29 10:16:23 +0200

    +++

--- jouer_quizz.php
+++ jouer_quizz.php
@@ -60,45 +60,64 @@ $result = mysqli_query($connect_bdd, $test);
             <h2>Liste des questions</h2>
         </div>
         <div class="container">
-            <div class="border border-secondary rounded">
-                <?php   
-                    if ($result->num_rows > 0) {
-                    // Parcourir les questions
-                        while ($row = $result->fetch_assoc()) {
-                            $questionId = $row['id_question'];
-                            $questionText = $row['intitule'];
+        <form method="post" action="">
+            <?php
+            if ($result->num_rows > 0) {
+                $questionIndex = 0; // Indice pour suivre la question actuelle
+                // Parcourir les questions
+                while ($row = $result->fetch_assoc()) {
+                    $questionId = $row['id_question'];
+                    $questionText = $row['intitule'];
 
                     // Sélection des réponses pour la question actuelle
-                            $sql = "SELECT * FROM choices WHERE id_question='$questionId'";
-                            $resulte = mysqli_query($connect_bdd, $sql);
-                ?>
-                <div class="card bg-light">
-                    <div class="cache">
+                    $sql = "SELECT * FROM choices WHERE id_question='$questionId'";
+                    $resulte = mysqli_query($connect_bdd, $sql);
+                    ?>
+                    <div class="card bg-light <?php echo ($questionIndex > 0) ? 'hidden' : ''; ?>">
                         <div class="card-header">
-                            <?php echo "<h2>$questionText</h2>"; ?>
+                            <?php echo $questionText; ?>
                         </div>
-                            <div class="card-body">
-                                <?php
-                                    if ($resulte->num_rows > 0) {
-                                    // Parcourir les réponses de la question actuelle
-                                        while ($row = $resulte->fetch_assoc()) {
-                                            echo $row["bonne_reponse"] . "<br>" . $row["reponse"] . "<br>" . $row["reponce"] . "<br>" . $row["reponze"];
-                                        }
-                                    } else {
-                                        echo "Aucune réponse trouvée.";
-                                    }
-                                ?>
-                                </div>
-                            </div>
+                        <div class="card-body">
+                            <?php
+                            if ($resulte->num_rows > 0) {
+                                // Parcourir les réponses de la question actuelle
+                                while ($row = $resulte->fetch_assoc()) {
+                                    $responseId = $row["id_choice"];
+                                    $responseText = $row["bonne_reponse"];
+                                    $responseText1 = $row["reponse"];
+                                    $responseText2 = $row["reponce"];
+                                    $responseText3 = $row["reponze"];
+                                    ?>
+                                    <input type="radio" name="bonne_reponse<?php echo $questionIndex; ?>" value="<?php echo $responseId; ?>">
+                                    <label><?php echo $responseText; ?></label>
+                                    <br>
+                                    <input type="radio" name="reponse<?php echo $questionIndex; ?>" value="<?php echo $responseId; ?>">
+                                    <label><?php echo $responseText1; ?></label>
+                                    <br>
+                                    <input type="radio" name="reponce<?php echo $questionIndex; ?>" value="<?php echo $responseId; ?>">
+                                    <label><?php echo $responseText2; ?></label>
+                                    <br>
+                                    <input type="radio" name="reponze<?php echo $questionIndex; ?>" value="<?php echo $responseId; ?>">
+                                    <label><?php echo $responseText3; ?></label>
+                                    <br>
+                                    <?php
+                                }
+                            } else {
+                                echo "Aucune réponse trouvée.";
+                            }
+                            ?>
                         </div>
-                <?php
-                    }
-                    } else {
-                        echo "Aucune question trouvée.";
-                    }
+                    </div>
+                    <?php
+                    $questionIndex++; // Augmenter l'indice de la question actuelle
+                }
                 ?>
-                </div>
-            </div>
-        </div>
+                <input type="button" value="Valider" onclick="afficherProchaineQuestion()">
+            <?php
+            } else {
+                echo "Aucune question trouvée.";
+            }
+            ?>
+        </form>
     </body>
 </html>
