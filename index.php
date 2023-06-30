d052377afd19ec261e6b9fdb602fffd5459cc0d3
Babacar2804 <136070296+Babacar2804@users.noreply.github.com>
2023-06-30 10:13:07 +0200

    ajout

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ include 'header.php';
                         <div class="para">                  
                             <h2>Nous Connaitre</h2>
                             <br>
-                            <p> Nous sommes une équipe de trois développeurs ayant crée ce site en un mois pour M.Peon <br><br> Dans l'équipe il y a Stive, 23 ans, le crack de la BDD et du PHP. <br><br> Il y a aussi Babacar, 19 ans, il s'occupe très bien du JS (on le complimente pas trop sinon il prends le melon ^^). <br><br> Et Finalement, Paul, 25ans qui s'occupe pas trop mal du css et de bootstrap.</p>
+                            <p> Nous sommes une équipe de trois développeurs ayant crée ce site en un mois pour M.Peon <br><br> Dans l'équipe il y a Stive, 23 ans, le crack de la BDD et du PHP. <br><br> Il y a aussi Babacar, 19 ans, il s'occupe très bien du JS. <br><br> Et Finalement, Paul, 25ans qui s'occupe pas trop mal du css et de bootstrap.</p>
                         </div>
                     </div>
                 </div>
