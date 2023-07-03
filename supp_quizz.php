2a72a2d4e8edc2af4ac4cc6f600fc82f294008a3
Numen <stivegamy@outlook.fr>
2023-07-03 10:43:53 +0200

    +++

--- supp_quizz.php
+++ supp_quizz.php
@@ -23,7 +23,7 @@ if (isset($_GET["id_quizz"])) {
     $deleteQuery = "DELETE FROM quizzes WHERE id_quizz = '$id'";
     mysqli_query($conn, $deleteQuery);
 
-    header("location: admin_edit_quizz.php");
+    header("location: admin.php");
     exit();
 }
 
