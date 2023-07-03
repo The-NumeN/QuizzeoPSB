2254df71aa512887648b50bb3b5027dae5028898
Numen <stivegamy@outlook.fr>
2023-07-03 12:04:22 +0200

    +++

--- jouer_quizz.php
+++ jouer_quizz.php
@@ -31,7 +31,7 @@ $result = mysqli_query($connect_bdd, $test);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Connexion</title>
+    <title>Jouer</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="connect.css">
