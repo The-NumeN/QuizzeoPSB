9d9727bbc202cfca628d83609586b92d984dda39
Babacar2804 <136070296+Babacar2804@users.noreply.github.com>
2023-07-03 10:41:10 +0200

    ajout

--- jouer_quizz.php
+++ jouer_quizz.php
@@ -1,5 +1,20 @@
 <?php
+
 session_start();
+if (isset($_POST['logout']) && $_POST['logout'] === 'true') {
+    // Détruire la session
+    session_destroy();
+
+    // Rediriger vers la page de connexion
+    header("location: Connexion.php");
+    exit();
+}
+// Vérifier si l'utilisateur est connecté, sinon rediriger vers la page de connexion
+if (!isset($_SESSION["id_test"]) || !isset($_SESSION["pseudo"])) {
+    header("location: Connexion.php");
+    exit();
+    
+}
 $connect_bdd = mysqli_connect("127.0.0.1", "root", "", "quizzeo");
 $id_test = $_SESSION['id_test'];
 $id_quizz = $_GET['id_quizz'];
@@ -19,9 +34,34 @@ $result = mysqli_query($connect_bdd, $test);
     <title>Connexion</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="connect2.css">
+    <link rel="stylesheet" href="connect.css">
 </head>
-
+<body>
+<nav class="navbar navbar-expand navbar-dark bg-dark">
+            <div class="container-fluid">
+            <!-- ajout du logo (retour au menu principal lorsque l'on clique dessus) -->
+                <a href="index.php"><img class="navbar-brand" src="img/logo-quiz-symboles-bulle-dialogue-concept-spectacle-questionnaire-chante-bouton-quiz-concours-questions-examen-embleme-moderne-interview_180786-72.avif" width="75" height="75" class="d-inline-block align-center" alt="Erreur"></a>
+        
+                <div class="navbar" id="navbarNav">
+                    <ul class="navbar-nav  ">
+          <!-- ajout des liens de redirection -->
+                        <div class="inscri">
+                            <li class="nav-item">
+                                <br><p class="bonjour">Bonjour <span><?php echo ucfirst($_SESSION["pseudo"]); ?></span>, Bienvenue !</p>
+                            </li>
+                        </div>
+                        <div class="form-inline">
+                            <li class="nav-item">
+                            <form action="" method="post">
+                                    <input type="hidden" name="logout" value="true">
+                                    <div class="tamere"><button class="deco"><img src="img\portal.png"width="60px" height="60px" class="d-inline-block align-center" alt=""></button></div>
+                                </form>
+                            </li>
+                        </div>
+                    </ul>
+                </div>
+            </div>
+        </nav>
 <div class="cuicuiz">
     <div class="liquest">
         <h2>Liste des questions</h2>
@@ -117,5 +157,6 @@ $result = mysqli_query($connect_bdd, $test);
         <script src="scripts.js"></script>
 
     </div>
-</div>
+    </div>
+</body>
 </html>
