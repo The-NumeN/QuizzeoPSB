9fa435e03a0c640ecc26b177161eb1188040e2f0
Babacar2804 <136070296+Babacar2804@users.noreply.github.com>
2023-07-03 11:32:01 +0200

    ajout

--- Scripts.js
+++ Scripts.js
@@ -6,7 +6,7 @@ function addquest() {
 
   var addquestHTML = document.getElementById('crea').innerHTML;
 
-  addquestHTML += '<div class="id_question"><br><form method="post"><input type="text" name="intitule' + ident + '" placeholder="Intitulé"><br><input type="text" name="bonne_reponse' + ident + '" placeholder="Bonne réponse"><br><input type="text" name="reponse1-' + ident + '" placeholder="Mauvaise réponse "><br><input type="text" name="reponse2-' + ident + '" placeholder="Mauvaise réponse"><br><input type="text" name="reponse3-' + ident + '" placeholder="Mauvaise réponse"><br><input type="hidden" name="ident" value="' + ident + '"></form></div>';
+  addquestHTML += '<div class="id_question"><br><form method="post"><input type="text" name="intitule' + ident + '" placeholder="Intitulé" required><br><input type="text" name="bonne_reponse' + ident + '" placeholder="Bonne réponse"required><br><input type="text" name="reponse1-' + ident + '" placeholder="Mauvaise réponse"required><br><input type="text" name="reponse2-' + ident + '" placeholder="Mauvaise réponse"required><br><input type="text" name="reponse3-' + ident + '" placeholder="Mauvaise réponse"required><br><input type="hidden" name="ident" value="' + ident + '"required></form></div>';
 
   document.getElementById('crea').innerHTML = addquestHTML;
 }
