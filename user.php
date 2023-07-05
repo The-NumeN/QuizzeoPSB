929fb41a7c1ac1e508e3c5705e9582d4323cc8c5
Babacar2804 <136070296+Babacar2804@users.noreply.github.com>
2023-07-05 11:21:14 +0200

    ajout

--- user.php
+++ user.php
@@ -33,14 +33,9 @@ while ($row = mysqli_fetch_assoc($result)) {
 <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>User</title>
-<<<<<<< HEAD
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-        <link rel="stylesheet" href="connectE.css">
-=======
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="connect2.css">
->>>>>>> 533ecb908bc03558a016ddb7b9548d625080c067
+
     </head>
 
     <body>
