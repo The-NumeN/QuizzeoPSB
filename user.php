4e5a62915f5f3e29608d0b5d70f0fe01e8603445
poulpy666 <lilopolo912@gmail.com>
2023-07-05 21:17:36 +0200

    nada

--- user.php
+++ user.php
@@ -47,62 +47,63 @@ while ($row = mysqli_fetch_assoc($result)) {
         </style>
         <!-- HEADER -->
         <div class="hed">
-        <nav class="navbar navbar-expand navbar-dark bg-dark">
-            <div class="container-fluid">
+            <nav class="navbar navbar-expand navbar-dark bg-dark">
+                <div class="container-fluid">
             <!-- ajout du logo (retour au menu principal lorsque l'on clique dessus) -->
-                <a href="index.php"><img class="navbar-brand" src="img/logo-quiz-symboles-bulle-dialogue-concept-spectacle-questionnaire-chante-bouton-quiz-concours-questions-examen-embleme-moderne-interview_180786-72.avif" width="75" height="75" class="d-inline-block align-center" alt="Erreur"></a>
-                <div class="navbar" id="navbarNav">
-                    <ul class="navbar-nav  ">
+                    <a href="index.php"><img class="navbar-brand" src="img/logo-quiz-symboles-bulle-dialogue-concept-spectacle-questionnaire-chante-bouton-quiz-concours-questions-examen-embleme-moderne-interview_180786-72.avif" width="75" height="75" class="d-inline-block align-center" alt="Erreur"></a>
+                    <div class="navbar" id="navbarNav">
+                        <ul class="navbar-nav  ">
             <!-- ajout des liens de redirection -->
-                        <div class="inscri">
-                            <li class="nav-item">
-                                <br><p class="bonjour">Compte de <span><?php echo ucfirst($_SESSION["pseudo"]); ?></span></p>
-                            </li>
-                        </div>
-                        <div class="form-inline">
-                            <li class="nav-item">
-                                <form action="" method="post">
-                                    <input type="hidden" name="logout" value="true">
-                                    <div class="tamere"><button class="deco"><img src="img\portal.png"width="60px" height="60px" class="d-inline-block align-center" alt=""></button></div>
-                                </form>                            
-                            </li>
-                        </div>
-                    </ul>
+                            <div class="inscri">
+                                <li class="nav-item">
+                                    <br><p class="bonjour">Compte de <span><?php echo ucfirst($_SESSION["pseudo"]); ?></span></p>
+                                </li>
+                            </div>
+                            <div class="form-inline">
+                                <li class="nav-item">
+                                    <form action="" method="post">
+                                        <input type="hidden" name="logout" value="true">
+                                        <div class="tamere"><button class="deco"><img src="img\portal.png"width="60px" height="60px" class="d-inline-block align-center" alt=""></button></div>
+                                    </form>                            
+                                </li>
+                            </div>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
         </div><br><br>
-        <div class="container">
-            <br>
-            <div class="border border-dark rounded w-75 mx-auto">
-                <br>
+        <div class="container"><br>
+            <div class="border border-dark rounded w-75 mx-auto"><br>
                 <div class="row">
                     <?php foreach ($quizzes as $quiz) : ?>
                         <div class="col-xl-4 col-lg-4">
                             <div class="zoom">
                                 <span class="intil"><?php echo $quiz['titre']; ?></span><br><br>
-                                <span><?php $difficulte = $quiz['difficulte'];
-                                                $difficulteText = "";
-                                                if ($difficulte == 1) {
-                                                    $difficulteText = "Facile";
-                                                } elseif ($difficulte == 2) {
-                                                    $difficulteText = "Moyen";
-                                                } elseif ($difficulte == 3) {
-                                                    $difficulteText = "Difficile";
-                                                }
-                                                echo $difficulteText;
-                                            ?>
+                                <span>
+                                    <?php 
+                                        $difficulte = $quiz['difficulte'];
+                                        $difficulteText = "";
+                                        if ($difficulte == 1) {
+                                            $difficulteText = "Facile";
+                                        } elseif ($difficulte == 2) {
+                                            $difficulteText = "Moyen";
+                                        } elseif ($difficulte == 3) {
+                                            $difficulteText = "Difficile";
+                                        }
+                                        echo $difficulteText;
+                                    ?>
                                 </span><br><br><br>                                                      
                                     <a href="jouer_quizz.php?id_quizz=<?php echo $quiz['id_quizz']; ?>"><button>Jouer</button></a><br>
                                 <br><br>
                             </div>
                         </div>
-                    </div>
+                        <?php endforeach; ?>
+                </div>
                     <br>
-                <?php endforeach; ?>
+                </div>
             </div>
         </div>  
-            <br><br><br><br>
+            <!-- <br><br><br><br> -->
             <footer class="fixed_footer">
   <div class="content">
     <p>&copy; - Stive Gamy  -  Babacar Gueye -  Paul Vicens </p>
